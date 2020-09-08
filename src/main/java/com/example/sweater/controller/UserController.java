a4afd835dc24a3328e24ddb179b28bf1c853c3cc
grachev nikolay <Grachev.Nikolay79@gmail.com>
2020-09-08 16:55:56 +0300

    начало добавления авторизации

---
+++ src/main/java/com/example/sweater/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.example.sweater.controller;
+
+import com.example.sweater.domain.Role;
+import com.example.sweater.domain.User;
+import com.example.sweater.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/user")
+@PreAuthorize("hasAuthority('ADMIN')")
+public class UserController {
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping
+    public String userList(Model model) {
+        model.addAttribute("users", userRepo.findAll());
+        return "userList";
+    }
+
+    @GetMapping("{user}")
+    public String userEditForm(@PathVariable User user, Model model) {
+    //public String userEditForm(Long userid, Model model) {
+        //model.addAttribute("user", userRepo.findById(userid));
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        return "userEdit";
+    }
+}
