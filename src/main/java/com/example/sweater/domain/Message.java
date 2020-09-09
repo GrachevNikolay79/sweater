b8bc066b955cd77beabf4ecbcb287e721e895093
grachev nikolay <Grachev.Nikolay79@gmail.com>
2020-09-09 16:29:03 +0300

    Добавлена загрузка картинок и отдача статики (*.css и т.п.)

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -17,6 +17,8 @@ public class Message {
     @JoinColumn(name = "user_id")
     private User author;
 
+    private String filename;
+
     public Message(){
     }
 
@@ -56,5 +58,13 @@ public class Message {
 
     public void setAuthor(User author) {
         this.author = author;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
     }
 }
